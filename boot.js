192a65496bffdb6b39e88f82adb9a230ff55a045
Ardhi Lukianto <ardhi@lukianto.com>
2019-01-11 11:28:15 +0700

    reg upate

--- boot.js
+++ boot.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = function (cuk) {
-  const { _, helper, config } = cuk.pkg.core.lib
-  const cfg = config('bootstrap')
+  const { _, helper } = cuk.pkg.core.lib
+  const cfg = helper('core:config')('bootstrap')
   return new Promise((resolve, reject) => {
     _.each(helper('core:pkgs')(), p => {
       let themes = _.get(p, 'cfg.cuks.bootstrap.themes')
